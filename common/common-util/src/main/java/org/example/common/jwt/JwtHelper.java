cc578ef20d725d30e0cc614b7a4643573297d778
Yuliang28 <3125016358@qq.com>
2023-04-07 14:22:04 +0800

    add jwt

---
+++ common/common-util/src/main/java/org/example/common/jwt/JwtHelper.java
@@ -0,0 +1,61 @@
+package org.example.common.jwt;
+
+import io.jsonwebtoken.*;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+public class JwtHelper {
+
+    // token有效期
+    private static long tokenExpiration = 365 * 24 * 60 * 60 * 1000;
+    // 签名
+    private static String tokenSignKey = "123456";
+
+
+    // 根据用户ID和名称生成token
+    public static String createToken(Long userId, String username) {
+        String token = Jwts.builder()
+                // 分类
+                .setSubject("AUTH-USER")
+                // 有效期
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
+                // 主体
+                .claim("userId", userId)
+                .claim("username", username)
+                // 签名
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey)
+                // 压缩
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+        return token;
+    }
+
+    // 从token获取UserId
+    public static Long getUserId(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return null;
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            Integer userId = (Integer) claims.get("userId");
+            return userId.longValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    // 从token获取UserName
+    public static String getUsername(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return "";
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            return (String) claims.get("username");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
