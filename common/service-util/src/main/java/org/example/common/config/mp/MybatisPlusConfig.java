6b7109a3214f8d071d4451b61195391f39fb1e50
Yuliang28 <3125016358@qq.com>
2023-04-13 17:39:37 +0800

    fix config

--- common/service-util/src/main/java/org/example/common/config/mp/MybatisPlusConfig.java
+++ common/service-util/src/main/java/org/example/common/config/mp/MybatisPlusConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("org.example.auth.mapper")
+@MapperScan("org.example.*.mapper")
 public class MybatisPlusConfig {
 
     /**
