245b998e65b6f345a834cdb30b3ab5d8f176a002
Yuliang28 <3125016358@qq.com>
2023-04-13 17:30:11 +0800

    add user mapper

--- service-oa/src/main/java/org/example/auth/mapper/SysUserMapper.java
+++ service-oa/src/main/java/org/example/auth/mapper/SysUserMapper.java
@@ -1,9 +1,11 @@
 package org.example.auth.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.example.model.system.SysUser;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
 }
