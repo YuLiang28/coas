5f6fd90fe050527f781bf607f126ce8eef4bd8f5
Yuliang28 <3125016358@qq.com>
2023-04-13 17:34:31 +0800

    add user service function

--- service-oa/src/main/java/org/example/auth/service/SysUserService.java
+++ service-oa/src/main/java/org/example/auth/service/SysUserService.java
@@ -4,7 +4,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import org.example.model.system.SysUser;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public interface SysUserService extends IService<SysUser> {
     void updateStatus(Long id, Integer status);
+
+    SysUser getUserByUsername(String username);
+
+    Map<String, Object> getCurrentUser();
 }
--- service-oa/src/main/java/org/example/auth/service/impl/SysUserServiceImpl.java
+++ service-oa/src/main/java/org/example/auth/service/impl/SysUserServiceImpl.java
@@ -1,13 +1,18 @@
 package org.example.auth.service.impl;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.example.auth.mapper.SysUserMapper;
 import org.example.auth.service.SysUserService;
 import org.example.model.system.SysUser;
+import org.example.security.custom.LoginUserInfoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
@@ -18,11 +23,28 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> impl
     @Override
     public void updateStatus(Long id, Integer status) {
         SysUser sysUser = this.getById(id);
-        if(status.intValue() == 1) {
+        if (status == 1) {
             sysUser.setStatus(status);
         } else {
             sysUser.setStatus(0);
         }
         this.updateById(sysUser);
     }
+
+    @Override
+    public SysUser getUserByUsername(String username) {
+        LambdaQueryWrapper<SysUser> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysUser::getUsername, username);
+        return baseMapper.selectOne(wrapper);
+    }
+
+    @Override
+    public Map<String, Object> getCurrentUser() {
+        SysUser sysUser = sysUserMapper.selectById(LoginUserInfoHelper.getUserId());
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", sysUser.getName());
+        map.put("phone", sysUser.getPhone());
+        return map;
+    }
+
 }
