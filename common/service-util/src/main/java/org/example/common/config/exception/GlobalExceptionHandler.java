05f381de96c97d6429793836ec1e1c3220f1cd8f
Yuliang28 <3125016358@qq.com>
2023-04-13 17:26:05 +0800

    add no access exception handling

--- common/service-util/src/main/java/org/example/common/config/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/org/example/common/config/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.example.common.config.exception;
 
 import org.example.common.result.Result;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,5 +17,10 @@ public class GlobalExceptionHandler {
     public Result error(Exception e){
         e.printStackTrace();
         return Result.fail(e.getMessage()).message("出现异常");
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public Result AccessDeniedError(AccessDeniedException e){
+        return Result.fail(e.getMessage()).code(209).message("当前用户无访问权限");
     }
 }
