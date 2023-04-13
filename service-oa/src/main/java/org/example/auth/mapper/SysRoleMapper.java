14dcc6d9dd571e9da29083633c06fcfde1244879
Yuliang28 <3125016358@qq.com>
2023-04-13 17:30:21 +0800

    add role mapper

--- service-oa/src/main/java/org/example/auth/mapper/SysRoleMapper.java
+++ service-oa/src/main/java/org/example/auth/mapper/SysRoleMapper.java
@@ -1,9 +1,11 @@
 package org.example.auth.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.example.model.system.SysRole;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Mapper
 public interface SysRoleMapper extends BaseMapper<SysRole> {
 }
